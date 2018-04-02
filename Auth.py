ed80dbc836c2b119cc760673b7747f032c1c1183
DNA386 <37022011+DNA386@users.noreply.github.com>
2018-04-02 16:06:47 +0100

    Update Auth.py

--- Auth.py
+++ Auth.py
@@ -1,58 +1,50 @@
-import string
-from random import *
+import requests
 from flask import Flask, url_for, request, render_template, session, escape, redirect, Blueprint
-from google.oauth2 import id_token
+from google.oauth2 import id_token, credentials
 from google.auth.transport import requests
 
 # Declare blueprint:
-Auth = Blueprint('Auth',__name__, url_prefix='/auth', template_folder='templates')
+Auth = Blueprint('Auth',__name__, url_prefix='/auth', template_folder='modules/templates')
 
-# randomly generate the secret key for sessions:
-allchar = string.ascii_letters + string.punctuation + string.digits
-Auth.secret_key  = "".join(choice(allchar) for x in range(32))
+CLIENT_ID = '395319603732-s2ictf4jgnfj5c7ra1dm64oatnrmebqf.apps.googleusercontent.com'	#team10 account
 
 @Auth.route('/')
 def index():
-    if 'username' in session:
-        return 'Logged in.'
-    return 'You are not logged in.'
+	if 'user' in session:
+		return ('Logged in as ' + session['user']['name'])
+	return 'You are not logged in.'
 
 @Auth.route('/login', methods=['GET','POST'])
 def login():
 	if request.method == 'POST':
+		if request.form:
+			token = request.form['idtoken']
+			try:
+				# Specify the CLIENT_ID of the app that accesses the backend:
+				idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
 
-		token = request['idtoken']
-		CLIENT_ID = "321008678947-bd2hvj2snrr96if8ec34vvecd2rmmcdk.apps.googleusercontent.com"
+				if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+					raise ValueError('Wrong issuer.')
 
-		try:
-			# Specify the CLIENT_ID of the app that accesses the backend:
-			idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
-
-			if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-				raise ValueError('Wrong issuer.')
-
-			# ID token is valid. Get the user's Google Account ID from the decoded token.
-			userId = idinfo['sub']
-
-		except ValueError:
 			# Invalid token
-			pass
+			except ValueError:
+				return 'invalid token'
 
-		# log the user in
-		session['username'] = userId
-		# send them to a member page --todo: this will presumably be a page dealt with by another part of the app
-		return redirect(url_for(Auth.members))
-	
+			# log the user in on the server
+			session['user'] = {
+					'userId': idinfo['iss'],
+					'name': idinfo['name']
+				}
+
+			return session['user']['name']
+
 	# Invalid token or get request - send to login page
 	return (render_template("login_googleOauth.html"))
 
-@Auth.route('/members')
-def members():
-	if 'username' in session:
-		return render_template('member.html')
-
 @Auth.route('/logout')
 def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for(Auth.index))
+	# remove user from session (log them out)
+	if 'user' in session:
+		session.pop('user', None)
+		return 'logged out from server'
+	return 'not logged in'
