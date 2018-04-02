1e63a4de195064f0cd268224554f7c3ede335976
DNA386 <37022011+DNA386@users.noreply.github.com>
2018-04-02 20:11:15 +0100

    backend.py is now root for the api

--- backend.py
+++ backend.py
@@ -1,68 +1,25 @@
-import requests
-import json
-import datetime
-import time
 import string
-
-from flask import Flask, render_template
-from flask import request, jsonify
-from flask_restful import Resource, Api
-from Auth import Auth
 from random import *
+from flask import Flask, Blueprint, render_template
+
+# import api modules
+from api.Auth import Auth
+from api.Scheduling import Scheduling
 
 app = Flask(__name__)
-api = Api(app)
 
+# Link api modules to main app
 app.register_blueprint(Auth)
+app.register_blueprint(Scheduling)
 
-# randomly generate the secret key for user sessions:
+# randomly generate the secret key for sessions:
 allchar = string.ascii_letters + string.punctuation + string.digits
 app.secret_key  = "".join(choice(allchar) for x in range(32))
 
-headers = {
-  'Accept': 'application/json'
-}
-
-#this code would get, from the browser, the power of a device and the number of minutes the user wants to use it
-#it will give a webpage with the power used, the least carbon produced and the optimal plug-in time
-
+# Render the homepage
 @app.route('/')
 def home():
 	return render_template('index.html')
-
-
-@app.route('/server', methods = ['GET', 'POST'])
-
-class BestTime24h(Resource):
-	def get_data():		#this finds the current time and returns the data for the next 24h
-		currTime = time.strftime("%Y-%m-%dT%H:%MZ")
-		link = 'https://api.carbonintensity.org.uk/intensity/' + currTime + '/fw24h'
-		r = requests.get(link, params={}, headers = headers)
-		data = json.loads(r.text)
-		return data
-	
-	def get(self, powerGET, timeMinGET):	
-		power = float(powerGET)			#get the power of the device
-		timeMin = int(timeMinGET)		#get the number of minutes it should work
-		#power = 10
-		#timeMin = 135
-		kwh = timeMin/60 * power		#compute the energy consumed in that time
-		intervals = timeMin//30			#compute the number of 30 minutes intervals
-		data = BestTime24h.get_data()	#get the data for the next 24h from now
-		minCarbon = 1000000
-		minCarbonTime = data['data'][0]['from']
-		for i in range (0, 48 - intervals):		#go through the data and choose a period with the minimum carbon consumed
-			carbon = 0
-			for j in range (0, intervals):
-				carbon += data['data'][i + j]['intensity']['forecast'] * power * 0.5
-			carbon += data['data'][i + intervals]['intensity']['forecast'] * power * (timeMin - 30 * intervals) / 30 * 0.5
-			if carbon < minCarbon:
-				minCarbon = carbon
-				minCarbonTime = data['data'][i]['from']
-		result = {'data': [{'enrgyConsumed': str(kwh)}, {'carbonProduced': str(minCarbon/kwh)}, {'plugInTime': str(minCarbonTime)}]}
-		return jsonify(result)	#return a json with the data	
-
-api.add_resource(BestTime24h, '/server/best24h/<powerGET>/<timeMinGET>')
 
 if __name__ == "__main__":
 	app.run()
