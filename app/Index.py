f5f350acabc055fec46c845f91489695dfb711d2
Zhenxian (Aaron) Yang <dev@zyang.me>
2018-04-05 19:54:05 +0100

    added static_folder in blueprint for accessing static assets

--- app/Index.py
+++ app/Index.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-Index = Blueprint('Index',__name__, template_folder='templates')
+Index = Blueprint('Index',__name__, template_folder='templates', static_folder='static')
 
 # Render the homepage
 @Index.route('/')
