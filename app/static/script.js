7a125333cc679b4682be3d25c8acecac3d611211
Alexandru Strimbu <strimbu_alexandru@yahoo.com>
2018-04-12 20:36:39 +0100

    Small change to script for showing date/time

--- app/static/script.js
+++ app/static/script.js
@@ -66,6 +66,9 @@ function writeschedule(data, m){
     plugDate = plugDate.substring(0,plugDate.length-1);
     var plugDateShort = plugDate.split(":")[0];
     var plugDateMins = plugDate.split(":")[1];
+    var plugDateTime = data.data[2].plugInTime
+    plugDateTime[10] = ' '
+    plugDateTime[16] = ' '
 
     var unplugShort = plugDateShort*1 + hours*1 + (1*plugDateMins + 1*mins)/60;
 
@@ -136,7 +139,7 @@ function writeschedule(data, m){
     			elements: {
 					center: {
 						text1: "You should plug in at ",
-						text2: data.data[2].plugInTime,
+						text2: plugDateTime,
           				fontStyle: 'Helvetica', // Default is Arial
          				 sidePadding: 20 // Defualt is 20 (as a percentage)
 				}
